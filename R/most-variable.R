7d1287b8437581056264e577330e2c7c2b8de279
Mark J Chen <mjchen.gene@gmail.com>
2018-11-13 08:04:23 -0600

    refactor: rename function and script to get most variably methylated loci.

--- R/most-variable.R
+++ R/most-variable.R
@@ -7,11 +7,9 @@
 #' @param top_n An integer of top N probes. Default to 1000.
 #' @return A matrix of beta values of most variably methylated loci.
 #' @export
-most_variably_methylated_loci <- function(x, top_n = 1000) {
+most_variable <- function(x, top_n = 1000) {
   if (missing(x))
     stop("Require argument x.")
-  beta_sd <- rowSds(x)
-  names(beta_sd) <- rownames(x)
-  keep <- rownames(beta_vals) %in% names(head(sort(beta_sd, decreasing = TRUE), top_n))
-  x[keep, ]
+  row_sd <- matrixStats::rowSds(x)
+  head(x[order(row_sd, decreasing = TRUE), ], n = top_n)
 }
