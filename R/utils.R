bcfd6ab4033e603efbacf6e66fbc7ea109c73e9d
Mark J Chen <mjchen.gene@gmail.com>
2018-11-13 08:29:30 -0600

    feat: add pipe operator.

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+# Helper functions
+
+#' Pipe
+#'
+#' Import pipe operator from magrittr
+#'
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @noRd
+#' @export
+#' @param lhs,rhs specify what lhs and rhs are
+NULL
