e32b0372dce89f4cadde55ac4e911080a19c5ff2
Mark J Chen <mjchen.gene@gmail.com>
2018-11-16 14:06:59 -0600

    feat: add the function to plot multiple tSNE results.

--- R/tsne.R
+++ R/tsne.R
@@ -37,3 +37,37 @@ plot_tsne <- function(tsne_res, ...) {
 }
 
 
+#' Plot multiple tSNE results.
+#'
+#' @param x A list of the returned value of \code{\link[RTsne]{RTsne}}
+#'   which itself is a list.
+#' @param pheno A character or factor vector.
+#' @param ... Any arguments passed to \code{\link[ggplot2]{aes}}
+#' @return A \code{\link[ggplot2]{ggplot}} object.
+multiplot_tsne <- function(x, pheno, ...) {
+  lapply(
+    seq(length(x)),
+    function(i) {
+      as.data.frame(x[[i]]$Y) %>%
+        dplyr::mutate(name = i) %>%
+        dplyr::mutate(pheno = pheno)
+    }) %>%
+    do.call(rbind, .) %>%
+    ggplot2::ggplot(., ggplot2::aes(
+      x = V1,
+      y = V2,
+      colour = pheno,
+      ...
+    )) +
+    ggplot2::geom_point() +
+    ggplot2::labs(x = "", y = "", colour = "Phenotype") +
+    ggplot2::scale_colour_brewer(palette = "Set2") +
+    ggplot2::facet_wrap(~name, scales = "free", ) +
+    ggplot2::theme_light() +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_blank(),
+      strip.background = ggplot2::element_blank(),
+      strip.text.x = ggplot2::element_blank()
+      )
+}
