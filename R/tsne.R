527e63d2e763a972d6c0e55acd533bbb57490ced
Mark J Chen <mjchen.gene@gmail.com>
2018-11-16 14:06:40 -0600

    feat: add the function to plot tSNE result.

--- R/tsne.R
+++ R/tsne.R
@@ -11,3 +11,29 @@ tsne <- function(x, n = 10, ...) {
     Rtsne::Rtsne(X = x, ...)
   })
 }
+
+
+#' Plot tSNE result.
+#'
+#' @param tsne_res The returned value of \code{\link[RTsne]{RTsne}},
+#'   which is a list.
+#' @param ... Any arguments passed to \code{\link[ggplot2]{aes}}
+#' @return A \code{\link[ggplot2]{ggplot}} object.
+plot_tsne <- function(tsne_res, ...) {
+  tsne_res$Y %>%
+    as.data.frame() %>%
+    ggplot2::ggplot(., ggplot2::aes(
+      x = V1,
+      y = V2,
+      ...
+    )) +
+    ggplot2::geom_point() +
+    ggplot2::labs(x = "", y = "", colour = "Phenotype") +
+    ggplot2::theme_light() +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_blank()) +
+    ggplot2::scale_colour_brewer(palette = "Set2")
+}
+
+
