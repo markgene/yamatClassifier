c20fa782f839cf4ae2f8cc2c08e447160b7676fd
Mark J Chen <mjchen.gene@gmail.com>
2018-11-16 12:05:26 -0600

    feat: add tests of find_pc_number* functions.

--- tests/testthat/test_pca.R
+++ tests/testthat/test_pca.R
@@ -11,19 +11,38 @@ test_that(
 )
 
 test_that(
-  "find_pc_number.capper()", {
-    res <- pca123(x = x, k = k)
-    set.seed(1)
-    expect_error(pc_capper <-
-                   find_pc_number.capper(
-                     x = x,
-                     eigen_values = res$eigs$values,
-                     k = k
-                   ),
-                 NA)
-    expect_true(pc_capper$variance_fraction - 0.8314244 < 1e-7)
+  "pca123()", {
+    expect_error(res <- pca123(x = x, k = k), NA)
   }
 )
+
+test_that("find_pc_number.var_frac()", {
+  res <- pca123(x = x, k = k)
+  set.seed(1)
+  expect_error(find_pc_number.var_frac(eigen_values = res$eigs$values),
+               NA)
+})
+
+test_that("find_pc_number.capper()", {
+  res <- pca123(x = x, k = k)
+  set.seed(1)
+  expect_error(pc_capper <-
+                 find_pc_number.capper(x = x,
+                                       eigen_values = res$eigs$values),
+               NA)
+  expect_true(pc_capper$variance_fraction - 0.8314244 < 1e-7)
+})
+
+test_that("find_pc_number.yamat()", {
+  res <- pca123(x = x, k = k)
+  set.seed(1)
+  expect_error(find_pc_number.yamat(
+    x = x,
+    eigen_values = res$eigs$values,
+    n = 3
+  ),
+  NA)
+})
 
 test_that(
   "shuffle_matrix.column()", {
