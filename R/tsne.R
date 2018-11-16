da75bbe86ab29aaf23e96bc52a1cbe0c5a6b50fa
Mark J Chen <mjchen.gene@gmail.com>
2018-11-16 13:20:45 -0600

    feat: add the function tsne() to run tSNE in batch mode.

--- R/tsne.R
+++ R/tsne.R
@@ -1,2 +1,13 @@
 # tSNE
 
+#' Run tSNE in batch mode.
+#'
+#' @param x A matrix.
+#' @param n An integer scalar. Default to 10.
+#' @param ... Any arguments passed to \code{\link[RTsne]{RTsne}}.
+#' @return A list of the returned value of \code{\link[RTsne]{RTsne}}.
+tsne <- function(x, n = 10, ...) {
+  lapply(seq(n), function(i) {
+    Rtsne::Rtsne(X = x, ...)
+  })
+}
