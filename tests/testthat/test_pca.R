962ffdb9fc757d413d638229e13e16418fbb07a3
Mark J Chen <mjchen.gene@gmail.com>
2018-11-16 10:34:09 -0600

    refactor: tests of shuffle matrix functions.

--- tests/testthat/test_pca.R
+++ tests/testthat/test_pca.R
@@ -1,9 +1,11 @@
 context("PCA")
 library(yamatClassifier)
 
+x <- as.matrix(mtcars[, c(1, 3:6)])
+k <- 3
+
 test_that(
   "shuffle_matrix.column()", {
-    x <- as.matrix(mtcars[, 1:4])
     expect_error(x_shuffled <- shuffle_matrix.column(x), NA)
     expect_true(all(x_shuffled[, 1] %in% x[, 1]))
   }
@@ -11,7 +13,6 @@ test_that(
 
 test_that(
   "shuffle_matrix.row()", {
-    x <- as.matrix(mtcars[, 1:4])
     expect_error(x_shuffled <- shuffle_matrix.row(x), NA)
     expect_true(all(x_shuffled[1, ] %in% x[1, ]))
   }
@@ -19,7 +20,6 @@ test_that(
 
 test_that(
   "shuffle_matrix()", {
-    x <- as.matrix(mtcars[, 1:4])
     expect_error(x_shuffled <- shuffle_matrix(x, "column"), NA)
     expect_true(all(x_shuffled[, 1] %in% x[, 1]))
     expect_error(x_shuffled <- shuffle_matrix(x, "row"), NA)
