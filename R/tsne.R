3bdf3a59304c3bd48ba4b16d23773f3ed4df7f62
Mark J Chen <mjchen.gene@gmail.com>
2018-11-16 15:57:04 -0600

    feat: add the function of diagnosing tSNE with coordinate trend.

--- R/tsne.R
+++ R/tsne.R
@@ -74,3 +74,42 @@ multiplot_tsne <- function(x, pheno, ...) {
       strip.text.x = ggplot2::element_blank()
       )
 }
+
+
+#' Diagnose multiple tSNE runs with coordinates plot.
+#'
+#' @param x A list of the returned value of \code{\link[RTsne]{RTsne}}
+#'   which itself is a list.
+#' @param pheno A character or factor vector.
+#' @param ... Any arguments passed to \code{\link[ggplot2]{aes}}
+#' @return A \code{\link[ggplot2]{ggplot}} object.
+#' @export
+diagnose_tsne.coord <- function(x, pheno, ...) {
+  df <- lapply(
+    seq(length(x)),
+    function(i) {
+      as.data.frame(x[[i]]$Y)
+    }) %>%
+    do.call(cbind, .)
+  p2 <- rep(seq(length(x)), each = 2)
+  p3 <- rep(c("X", "Y"), length(x))
+  colnames(df) <- paste0(p2, p3)
+  df$pheno <- pheno
+  df$name <- rownames(df)
+  # Plot
+  df %>%
+    tidyr::gather(key = "Coordinate", value = "Position", -pheno, -name) %>%
+    ggplot2::ggplot(., ggplot2::aes(
+      x = Coordinate,
+      y = Position,
+      colour = pheno,
+      group = name,
+      ...
+    )) +
+    ggplot2::geom_line() +
+    ggplot2::labs(x = "", y = "Position", colour = "Phenotype") +
+    ggplot2::scale_colour_brewer(palette = "Set2") +
+    ggplot2::theme_light()
+}
+
+
