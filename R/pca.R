fd3ede60431658736a1fbb39cefd7a9908cbb1a1
Mark J Chen <mjchen.gene@gmail.com>
2018-11-16 10:40:37 -0600

    fix: remove redundant parameter k from find_pc_number.capper().
    k should equal to the length of eigen_values.

--- R/pca.R
+++ R/pca.R
@@ -164,7 +164,6 @@ shuffle_matrix.row <- function(x) {
 #'   \code{vectors} of the item \code{eigs} of the returned value of
 #'   \code{\link{pca123}}, which is returned by
 #'   \code{\link[RSpectra]{eigs}}.
-#' @param k Number of eigenvalues requested.
 #' @return A list of three items:
 #'   \itemize{
 #'     \item \code{pca_num} An integer scalar of PC number.
@@ -173,7 +172,16 @@ shuffle_matrix.row <- function(x) {
 #'     \item \code{plot} A \code{\link[ggplot2]{ggplot}} object of
 #'       density plot of eigenvalues of observed and randomized data.
 #'   }
-find_pc_number.capper <- function(x, eigen_values, k = 50) {
+find_pc_number.capper <- function(x, eigen_values) {
+  if (missing(x))
+    stop("Argument x is required.")
+  if (!is.matrix(x))
+    stop("Argument x should be a matrix.")
+  if (missing(eigen_values))
+    stop("Argument eigen_values is required.")
+  if (!is.numeric(eigen_values))
+    stop("Argument eigen_values should be a numeric vector.")
+  k <- length(eigen_values)
   x_shuffled <- shuffle_matrix(x, margin = "col")
   res_shuffled <- pca123(x_shuffled, k = k)
   output <- list()
