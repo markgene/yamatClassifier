3f2afc6b553351de2f281be2dc087a7ff9b8f82c
Mark J Chen <mjchen.gene@gmail.com>
2024-09-16 03:10:51 -0400

    refact: rename Rscript

--- R/trainer.R
+++ R/trainer.R
@@ -4,7 +4,7 @@
 #' @param targets \code{data.frame} of targets. See \code{\link[minfi]{read.metharray.exp}}.
 #' @param output output directory.
 #' @param overwrite A bool if overwrite the result files.
-#' @return A S3 object of YamatClassifierTrainer class.
+#' @return A S3 object of \code{YamatClassifierTrainer} class.
 #' @export
 create_trainer <- function(idat_dir, targets, output, overwrite = FALSE) {
   if (is.null(targets$Sentrix_ID)) {
@@ -18,4 +18,19 @@ create_trainer <- function(idat_dir, targets, output, overwrite = FALSE) {
   )
   class(trainer) <- "YamatClassifierTrainer"
   return(trainer)
+}
+
+
+#' Get preprocessed file directory.
+#'
+#' @param trainer A S3 object of \code{YamatClassifierTrainer} class.
+#' @param dir_name output directory name.
+#' @param overwrite A bool if overwrite the result files.
+#' @return path of preprocessed file directory.
+get_preprocessed_dir <- function(trainer, dir_name = "dkfz_preprocessed") {
+  preprocessed_dir <- file.path(trainer$output, dir_name)
+  if (!dir.exists(preprocessed_dir)) {
+    dir.create(preprocessed_dir, recursive = TRUE)
+  }
+  return(preprocessed_dir)
 }
