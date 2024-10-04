6ae12a3379bc64750f1ef581eae5a96f9d4b23c7
Mark J Chen <mjchen.gene@gmail.com>
2024-10-04 10:56:59 -0400

    feat: test train model version 3

---
+++ tests/testthat/test_train_model3.R
@@ -0,0 +1,27 @@
+context("Test train_model3()")
+library(yamatClassifier)
+library(glmnet)
+library(randomForest)
+
+test_that("train_model3() end-to-end", {
+  require(datasets)
+  utils::data(iris)
+  result <- yamatClassifier::train_model3(
+    dat = iris,
+    response_name = "Species",
+    outer_cv_folds = 3,
+    inner_cv_folds = 3,
+    random_state = 1,
+    # mtry = 10,
+    top_n = 2,
+    selected_features = c("Sepal.Length", "Sepal.Width"),
+    importance = "permutation",
+    save_level = 2,
+    save_prefix = "train_model3",
+    overwrite = FALSE,
+    output = "output/train_model3",
+    verbose = TRUE
+  )
+  expect_true(is.list(result))
+  expect_true(length(result) == 1)
+})
