c2ce0197c443db9770e50df99c1183e145b64843
Mark J Chen <mjchen.gene@gmail.com>
2024-10-05 04:44:01 -0400

    feat: test train model 4

---
+++ tests/testthat/test_train_model4.R
@@ -0,0 +1,24 @@
+context("Test train_model4()")
+library(yamatClassifier)
+library(glmnet)
+library(randomForest)
+
+test_that("train_model4() end-to-end", {
+  require(datasets)
+  utils::data(iris)
+  tune_result <- yamatClassifier::train_model4(
+    dat = iris,
+    response_name = "Species",
+    outer_cv_folds = 3,
+    inner_cv_folds = 3,
+    random_state = 1,
+    mtry = 2,
+    save_level = 2,
+    save_prefix = "train_model4",
+    overwrite = FALSE,
+    output = "output/train_model4",
+    verbose = TRUE
+  )
+  expect_true(is.list(tune_result))
+  expect_true(length(tune_result) == 1)
+})
